7cfc73a5de7ddc088d4e531c0a45e14891fb4a2f
pink10000 <kyletrinh1223@gmail.com>
2024-04-13 21:29:48 -0700

    spi test

---
+++ spi/test1.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <fcntl.h>
+#include <unistd.h>
+
+int main() {
+    int fd;
+
+    // Open SPI device
+    fd = open("/dev/spidev0.0", O_WRONLY);
+    if (fd == -1) {
+        std::cerr << "Failed to open SPI device" << std::endl;
+        return 1;
+    }
+
+    // Write data to SPI device
+    char data[] = {0x37, 0xFF};
+    if (write(fd, data, sizeof(data)) == -1) {
+        std::cerr << "Failed to write to SPI device" << std::endl;
+        close(fd);
+        return 1;
+    }
+
+    // Close SPI device
+    if (close(fd) == -1) {
+        std::cerr << "Failed to close SPI device" << std::endl;
+        return 1;
+    }
+
+    return 0;
+}
